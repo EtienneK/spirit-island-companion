06239301e5fc6fee3497eef6010249c2db55b98d
Etienne Koekemoer <e@ek.pe>
2018-09-27 20:12:14 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import './App.css';
+
+// tslint:disable-next-line
+import { DB } from '../spirit-island-card-katalog/src/db';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React {DB.CARDS[0].name}</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
