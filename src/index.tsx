18094a1eb963fdbf7cd1a0c5dd5dc3b0be8caa8e
Etienne Koekemoer <e@ek.pe>
2018-09-30 08:07:19 +0200

    Added local storage

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,32 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
 import App from './App';
 import './index.css';
 import { rootReducer } from './redux';
 import registerServiceWorker from './registerServiceWorker';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
 const store = createStore(
-  rootReducer,
+  persistReducer(persistConfig, rootReducer),
   (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistStore(store)}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
+
 registerServiceWorker();
